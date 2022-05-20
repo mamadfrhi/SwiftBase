d4e0e7bee1fa55fb42930604bfb88e285e41362a
Mamad Farrahi <mamad.frhi@gmail.com>
2022-05-20 12:42:27 +0200

    project 1 created.

---
+++ Project1_StormViewer/Project1_StormViewer/Project1_StormViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1_StormViewer
+//
+//  Created by iMamad on 20.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
