70c7e5050d62632cdbf32e2d922e8009f0a8c194
Mamad Farrahi <mamad.frhi@gmail.com>
2022-05-25 18:40:21 +0200

    project init.

---
+++ Project5_WordScramble/Project5_WordScramble/Project5_WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5_WordScramble
+//
+//  Created by iMamad on 25.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
