979e63e23e0b176914dd21cb7430e47d2a10fed7
Mamad Farrahi <mamad.frhi@gmail.com>
2022-05-25 22:40:17 +0200

    implemented part #6.

--- Project5_WordScramble/Project5_WordScramble/Project5_WordScramble/ViewController.swift
+++ Project5_WordScramble/Project5_WordScramble/Project5_WordScramble/ViewController.swift
@@ -55,6 +55,9 @@ class ViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
         
+        var errorTitle: String
+        var errorMessage: String
+        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
@@ -62,9 +65,25 @@ class ViewController: UITableViewController {
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    return
+                } else { // isReal failed
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            } else { // is original failed
+                errorTitle = "Word already used"
+                errorMessage = "Be more original"
             }
+        } else {
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title!.lowercased())"
         }
+        
+        let ac = UIAlertController(title: errorTitle,
+                                   message: errorMessage,
+                                   preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK!", style: .default))
+        present(ac, animated: true)
     }
     
     func isPossible(word: String) -> Bool {
