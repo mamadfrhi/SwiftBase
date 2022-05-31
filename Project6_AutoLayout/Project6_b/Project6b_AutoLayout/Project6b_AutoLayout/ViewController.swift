b20c671dbaf37efcbdd9a33c24717f20853bf26f
Mamad Farrahi <mamad.frhi@gmail.com>
2022-05-31 18:19:34 +0200

    Project6_b base implemented.

---
+++ Project6_AutoLayout/Project6_b/Project6b_AutoLayout/Project6b_AutoLayout/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Project6b_AutoLayout
+//
+//  Created by iMamad on 31.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = .red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = .yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = .green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = .orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        let viewsDictionary = [
+            "label1" : label1,
+            "label2" : label2,
+            "label3" : label3,
+            "label4" : label4,
+            "label5" : label5,
+        ]
+        
+        for label in viewsDictionary.keys {
+            //                                                                           H means Horizontal NOT Height!
+            // | or pipe sign means edge of the view
+            // [] or brackets are edges of the view
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|",
+                                                               metrics: nil,
+                                                               views: viewsDictionary))
+        }
+    }
+
+
+}
+
