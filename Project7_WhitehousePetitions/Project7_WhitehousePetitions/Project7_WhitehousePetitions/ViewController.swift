ee175e432f3dfd67686940319045b3101e676cfa
Mamad Farrahi <mamad.frhi@gmail.com>
2022-06-01 16:03:14 +0200

    implemented part #3 of the tutorial.

--- Project7_WhitehousePetitions/Project7_WhitehousePetitions/Project7_WhitehousePetitions/ViewController.swift
+++ Project7_WhitehousePetitions/Project7_WhitehousePetitions/Project7_WhitehousePetitions/ViewController.swift
@@ -13,7 +13,22 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         
+        if let url = URL(string: urlString) {
+            if let jsonData = try? Data(contentsOf: url) {
+                parse(json: jsonData)
+            }
+        }
+    }
+    
+    private func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -22,8 +37,10 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Title goes here"
+        
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
 }
