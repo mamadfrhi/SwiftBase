ca0e8042a27383a014c9ba0344d71b1d2194a141
Mamad Farrahi <mamad.frhi@gmail.com>
2022-06-01 16:42:40 +0200

    part #5 of the tutorial implemented.

--- Project7_WhitehousePetitions/Project7_WhitehousePetitions/Project7_WhitehousePetitions/ViewController.swift
+++ Project7_WhitehousePetitions/Project7_WhitehousePetitions/Project7_WhitehousePetitions/ViewController.swift
@@ -13,13 +13,29 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString : String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
         
         if let url = URL(string: urlString) {
             if let jsonData = try? Data(contentsOf: url) {
                 parse(json: jsonData)
+                return
             }
         }
+        showError()
+    }
+    
+    private func showError() {
+        let ac = UIAlertController(title: "Loading Error",
+                                   message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     private func parse(json: Data) {
