c413e351184fe331918e83a8044a4b18d6f195cb
Mamad Farrahi <mamad.frhi@gmail.com>
2022-06-01 15:17:16 +0200

    base of the project 7 implemented

---
+++ Project7_WhitehousePetitions/Project7_WhitehousePetitions/Project7_WhitehousePetitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7_WhitehousePetitions
+//
+//  Created by iMamad on 01.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project7_WhitehousePetitions/README.md
@@ -0,0 +1,22 @@
+
+
+## Learned Concepts ☺️
+
+* UIAlertController
+* UIButton
+* CALayer
+* UIColor
+* Outlets
+* Asset catalogs
+* Interface Builder
+* Random numbers
+* Auto Layout
+* Actions
+ 
+
+
+## Showcase 📱
+
+
+https://user-images.githubusercontent.com/28094207/169885580-30ceb6f7-2055-4882-82dd-90dd1f417cac.mov
+
