31cad6562ab0614643e100c26c913a1cee97ec6c
Mamad Farrahi <mamad.frhi@gmail.com>
2022-06-01 15:29:04 +0200

    part #2 of the tutorial implemented.

--- Project7_WhitehousePetitions/Project7_WhitehousePetitions/Project7_WhitehousePetitions/ViewController.swift
+++ Project7_WhitehousePetitions/Project7_WhitehousePetitions/Project7_WhitehousePetitions/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Title goes here"
+        return cell
+    }
 }
 
