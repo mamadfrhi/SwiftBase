9b292501f620b54a94f89cbe3b8efdbdd53685ed
Mamad Farrahi <mamad.frhi@gmail.com>
2022-06-01 15:37:34 +0200

    models implemented.

---
+++ Project7_WhitehousePetitions/Project7_WhitehousePetitions/Project7_WhitehousePetitions/Petitions.swift
@@ -0,0 +1,12 @@
+ //
+//  Petitions.swift
+//  Project7_WhitehousePetitions
+//
+//  Created by iMamad on 01.06.22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
