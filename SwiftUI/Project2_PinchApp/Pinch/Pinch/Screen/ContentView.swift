9984c2c1965c130724c1a19035f619b2c675ea8e
Mohammad Farrahi <mohammad.farrahi@oculid.com>
2022-06-29 18:10:44 +0200

    double tap gesture implemented.

--- SwiftUI/Project2_PinchApp/Pinch/Pinch/Screen/ContentView.swift
+++ SwiftUI/Project2_PinchApp/Pinch/Pinch/Screen/ContentView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    @State private var isAnimating = false
+    @State private var imageScale : CGFloat = 1
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTETNT
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Color.red
+                    .ignoresSafeArea()
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.8), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                // MARK: - TAP GESTURE
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    }
+            } //: ZSTACK
+            .navigationTitle("Pinch and Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                isAnimating = true
+            }
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
+
+// MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
