0372443f17462138c0a2aa0870eaa475c68a539f
Mohammad Farrahi <mohammad.farrahi@oculid.com>
2022-06-29 15:43:30 +0200

    project resources added

---
+++ SwiftUI/Project2_PinchApp/Pinch/Pinch/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Mohammad Farrahi on 29.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
