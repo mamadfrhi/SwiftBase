06097412c532e7ce0b9de15a4b109f746df2c1f8
Mohammad Farrahi <mohammad.farrahi@oculid.com>
2022-07-05 14:01:38 +0200

    project 0 added.

---
+++ SwiftUI/0- SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Mohammad Farrahi on 17.06.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
