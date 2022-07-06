9986cc536a1b91ef496560269b086808edea5173
Mohammad Farrahi <mohammad.farrahi@oculid.com>
2022-07-06 16:41:47 +0200

    drag gesture implemented.

--- SwiftUI/Project2_PinchApp/Pinch/Pinch/Screen/ContentView.swift
+++ SwiftUI/Project2_PinchApp/Pinch/Pinch/Screen/ContentView.swift
@@ -12,8 +12,15 @@ struct ContentView: View {
     
     @State private var isAnimating = false
     @State private var imageScale : CGFloat = 1
+    @State private var imageOffset : CGSize = .zero
     
     // MARK: - FUNCTION
+    private func resetImageState() {
+        withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTETNT
     var body: some View {
@@ -29,20 +36,34 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.8), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
-                    .animation(.linear(duration: 1), value: isAnimating)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
-                // MARK: - TAP GESTURE
+                // MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2) {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
+                // MARK: - 1. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    withAnimation(.spring()) {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch and Zoom")
             .navigationBarTitleDisplayMode(.inline)
