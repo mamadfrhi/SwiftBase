177be23f1c2bb7ddd51737c315c02be0aa97521e
iMamad <mohammad.farrahi@oculid.com>
2022-08-29 16:56:01 +0200

    added SettingsRowView and extended SettingsRowView.

--- SwiftUI/3.Fruits/Fruits/Fruits/View/SettingsView.swift
+++ SwiftUI/3.Fruits/Fruits/Fruits/View/SettingsView.swift
@@ -40,12 +40,37 @@ struct SettingsView: View {
                             Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterool. Fruits are sources of many essential nuterients, including potassium, dietary fiber, vitamins, and much more.")
                                 .font(.footnote)
                         }
-                    }
+                    } //: BOX
                     .padding()
                     
                     // MARK: - SECTION 2
                     
                     // MARK: - SECTION 3
+                    
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "Application",
+                                              labelImage: "apps.iphone")
+                    ) {
+                        
+                        SettingsRowView(name: "Developer", content: "iMamad")
+                        SettingsRowView(name: "Designer", content: "Mamad Mamadian")
+                        SettingsRowView(name: "Compatibility", content: "iOS 14")
+                        
+                        SettingsRowView(name: "Developer",
+                                        linkLabel: "Contact",
+                                        linkDestination: "www.linkedin.com/in/imamad/")
+                        
+                        SettingsRowView(name: "Twitter",
+                                        linkLabel: "@iMamad_",
+                                        linkDestination: "www.twitter.com/iMamad_")
+                        
+                        SettingsRowView(name: "SwiftUI", content: "2.0")
+                        SettingsRowView(name: "Version", content: "1.1.0")
+                        
+                    } //: BOX
+                    .padding()
+                    
                 } //: VSTACK
                 .navigationBarTitle(Text("Settings"))
                 .navigationBarItems(trailing: Button(action: {
