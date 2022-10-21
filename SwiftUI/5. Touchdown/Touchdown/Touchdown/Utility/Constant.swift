92af4ad26b500a4f86ae772325979d810b5dc5d5
iMamad <mamad.frhi@gmail.com>
2022-10-21 17:38:39 +0200

    organised folders and added constant file.

---
+++ SwiftUI/5. Touchdown/Touchdown/Touchdown/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by iMamad on 21.10.22.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+let colorBackground : Color = Color("ColorBackground")
+let colorGray : Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
