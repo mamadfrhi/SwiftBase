cebfd857061562bf5ff044952a6cbc560e032b52
iMamad <mamad.frhi@gmail.com>
2022-10-21 17:17:54 +0200

    initialized the starter project.

---
+++ SwiftUI/5. Touchdown/Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by iMamad on 21.10.22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
