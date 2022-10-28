ae289091399ea75cf41fcbd9f62d71430b6fa0c0
iMamad <mamad.frhi@gmail.com>
2022-10-28 15:17:44 +0200

    implemented ContentView.

--- SwiftUI/5. Touchdown/Touchdown/Touchdown/App/ContentView.swift
+++ SwiftUI/5. Touchdown/Touchdown/Touchdown/App/ContentView.swift
@@ -11,8 +11,27 @@ struct ContentView: View {
     //MARK: PROPERTIES
     //MARK: - BODY
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack(spacing: 0) {
+                
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(.white)
+                    .shadow(color: .black.opacity(0.05),
+                            radius: 5,
+                            x: 0,
+                            y: 5)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            } //: VSTACK
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        } //: ZSTACK
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
@@ -22,3 +41,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+https://www.udemy.com/course/swiftui-masterclass-course-ios-development-with-swift/learn/lecture/24012580#overview
+tamom kardam
