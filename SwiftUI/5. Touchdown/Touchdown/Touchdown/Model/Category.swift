69c4b9e03e14a8405be0ddbf15b3813b230af59b
iMamad <mamad.frhi@gmail.com>
2022-10-28 18:53:08 +0200

    added Category model

---
+++ SwiftUI/5. Touchdown/Touchdown/Touchdown/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Touchdown
+//
+//  Created by iMamad on 28.10.22.
+//
+
+import Foundation
+
+struct Category: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
