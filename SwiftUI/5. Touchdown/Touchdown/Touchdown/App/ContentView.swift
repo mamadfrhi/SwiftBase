448d31dc5511af3c16dd949e87e7c2245412c1eb
iMamad <mamad.frhi@gmail.com>
2022-10-31 17:32:02 +0100

    added Brand View to the app.

--- SwiftUI/5. Touchdown/Touchdown/Touchdown/App/ContentView.swift
+++ SwiftUI/5. Touchdown/Touchdown/Touchdown/App/ContentView.swift
@@ -41,6 +41,10 @@ struct ContentView: View {
                         } //: GRID
                         .padding(15)
                         
+                        TitleView(title: "Brands")
+                        
+                        BrandGridView()
+                        
                         FooterView()
                             .padding(.horizontal)
                     } //: VSTACK
---
+++ SwiftUI/5. Touchdown/Touchdown/Touchdown/Model/Brand.swift
@@ -0,0 +1,13 @@
+//
+//  Brand.swift
+//  Touchdown
+//
+//  Created by iMamad on 31.10.22.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
